ae66d88e02cefb800fdc82ddb3d308b6eefdbcbf
Muhammad Zaigham <65202544+izaigham@users.noreply.github.com>
2021-03-19 21:52:52 +0500

    update HTML
    Remove dumy element form the HTML.
    and code in script file

---
+++ Project07/script.js
@@ -0,0 +1,13 @@
+// Grab DOM elements from HTML.
+
+const word = document.getElementById('word');
+const wrongLetters = document.getElementById('wrong-letters');
+const popup = document.getElementById('popup-conatiner');
+const message = document.getElementById('win-loss');
+const restartButton = document.getElementById('restart');
+const notitification = document.getElementById('slider-container');
+
+const handmanPart = document.querySelectorAll('.hangman-part');
+
+const wordPool = ['JavaScript','Computer','Facebook', 'Youtube','Hangman'];
+
