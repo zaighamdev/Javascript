1634adc0de3e6a3212553bc5201d91f64a5de34a
Muhammad Zaigham <65202544+izaigham@users.noreply.github.com>
2021-04-09 16:36:58 +0500

    Update script.js

--- Project07/script.js
+++ Project07/script.js
@@ -9,5 +9,35 @@ const notitification = document.getElementById('slider-container');
 
 const handmanPart = document.querySelectorAll('.hangman-part');
 
-const wordPool = ['JavaScript','Computer','Facebook', 'Youtube','Hangman'];
+// An array of words to select form
+const wordPool = ['javasript', 'computer', 'facebook', 'youtube', 'hangman'];
 
+// Selecting the word at random from the pool
+let selectWord = wordPool[Math.floor(Math.random() * wordPool.length)];
+
+// Arrays to classify the the input of the user
+const correctLetters = ['a', 'i', 'o', 'e', 'u', 'f'];
+const incorrectLetters = [];
+
+// Fucntion to display the word
+function displaySelectedWord() {
+    word.innerHTML = `
+        ${selectWord
+            .split('')
+            .map(
+                letter => `
+                    <span class="letter">
+                        ${correctLetters.includes(letter) ? letter : ''}
+                    </span>
+                `
+            )
+            .join('')
+        }
+
+    `;
+
+    const wordText = word.innerText.replace(/\n/g, '');
+    console.log(word.innerText);
+    console.log(wordText);
+}
+displaySelectedWord();
